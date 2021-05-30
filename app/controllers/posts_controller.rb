5fd853195582c0ccd2b1d498b6b502b83b77fe35
Asuka-I <1400cm@gmail.com>
2021-05-31 07:15:05 +0900

    update, destroy を作成

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,9 +1,12 @@
 class PostsController < ApplicationController
+  before_action :set_post, only: %i[edit update destroy]
+
   def index
     @posts = Post.includes(:user).order(:created_at)
   end
 
   def new
+    @post = Post.new
   end
 
   def create
@@ -19,14 +22,24 @@ class PostsController < ApplicationController
   end
 
   def update
+    @post.update!(post_params)
+    redirect_to @post
   end
 
   def destroy
+    @post.destroy!
+    redirect_to root_path
   end
 
   private
 
   def post_params
     params.require(:post).permit(:content)
+  end
+
+  def set_post
+    # 「自分の投稿」の中から URL の :id に対応する投稿を探す
+    # 「他人の投稿」の場合はエラーを出す
+    @post = current_user.posts.find(params[:id])
   end
 end
